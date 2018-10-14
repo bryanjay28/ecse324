39d1fa6777f743a9ae4470450f8acd5e31b6e664
bryanjay28 <bjay28@hotmail.com>
2018-10-14 19:54:07 -0400

    updated comments

--- G19_Lab1/center.s
+++ G19_Lab1/center.s
@@ -7,22 +7,22 @@ _start:		LDR R4, =RESULT		// holds the average location
 			LDR R0, [R2]		// holds the total of the numbers
 
 TOTAL:		SUBS R3, R3, #1		// decrement the counter
-			BEQ ENDTOTAL		//end loop if counter had reached 0
-			ADD R2, R2, #4
-			LDR R1, [R2]
-			ADD R0, R0, R1
+			BEQ ENDTOTAL		// end loop and go to ENDTOTAL if counter had reached 0
+			ADD R2, R2, #4		// point to the next address in the array
+			LDR R1, [R2]		// get the number located at the address
+			ADD R0, R0, R1		// add to the sum
 			B TOTAL
 
-ENDTOTAL: 	LDR R3, [R4, #4]	// holds the total number of values
+ENDTOTAL: 	LDR R3, [R4, #4]	// reset counter to hold total number of values
 			B AVERAGE
 
 AVERAGE:	LSR R3, R3, #1		// divide by 2 until counter hits 0
-			CMP R3, #0	
+			CMP R3, #0			// check if counter is 0
 			BEQ ENDAVERAGE	 	// if 0, then divide by 2 one more time then do substraction
 			LSR R0, R0, #1		// divide the total numbers by 2 to get avg
 			B AVERAGE
 
-ENDAVERAGE:	STR R0, [R4]
+ENDAVERAGE:	STR R0, [R4]		// store the average in RESULT
 			ADD R2, R4, #8		// holds the address of the first number in the list
 			LDR R3, [R4, #4]	// holds the total number of values
 			B SUBS_AVG
--- G19_Lab1/stddev.s
+++ G19_Lab1/stddev.s
@@ -9,30 +9,30 @@ _start:
 			LDR R5, [R4]		// holds the minimum number
 
 LOOP:
-			SUBS R3, R3, #1 //decrement the loop counter
-			BEQ COMPUTE_STD_DEV //end loop if counter had reached 0
-			ADD R2, R2, #4 //R2 points to next number in the list
-			LDR R1, [R2] // R1 holds the next number in the list
-			CMP R0, R1 //check if it is greater than the maximum
-			BGE MIN_LOOP //if no, branch to MIN_LOOP to check if its a min
-			MOV R0, R1 //if yes, update the current max
-			B LOOP // branch back to the loop
+			SUBS R3, R3, #1 	// decrement the loop counter
+			BEQ COMPUTE_STD_DEV // end loop if counter had reached 0
+			ADD R2, R2, #4 		// R2 points to next number in the list
+			LDR R1, [R2] 		// R1 holds the next number in the list
+			CMP R0, R1 			// check if it is greater than the maximum
+			BGE MIN_LOOP 		// if no, branch to MIN_LOOP to check if its a min
+			MOV R0, R1 			// if yes, update the current max
+			B LOOP 				// branch back to the loop
 
 MIN_LOOP:
 			CMP R5, R1
-			BGE LOOP //if no, branch, to LOOP and continue
-			MOV R5, R1 // if yes, update the current min
-			B LOOP //branch back to loop
+			BGE LOOP 			// if no, branch, to LOOP and continue
+			MOV R5, R1 			// if yes, update the current min
+			B LOOP 				// branch back to loop
 
 COMPUTE_STD_DEV:
 			SUBS R0, R0, R5
 			LSR R0, R0, #2
 			STR R0, [R4]
 
-END:	B END //infinite loop
+END:	B END 					// infinite loop
 
 
-RESULT:	 .word 0 // memory assigned for the result location 
-N:		 .word 7 // number of entrues in the list 
-NUMBERS: .word 4, 5, 3, 6 //the list data
+RESULT:	 .word 0 				// memory assigned for the result location 
+N:		 .word 7 				// number of entrues in the list 
+NUMBERS: .word 4, 5, 3, 6 		// the list data
 		 .word 1, 8, 2
