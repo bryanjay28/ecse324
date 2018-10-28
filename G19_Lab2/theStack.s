bd484a3ec3b8f7132c7e2bb9b7539a29fc26cee6
sarahswanson <35464873+sarahswanson@users.noreply.github.com>
2018-10-28 12:28:11 -0400

    Update theStack.s

--- G19_Lab2/theStack.s
+++ G19_Lab2/theStack.s
@@ -4,14 +4,14 @@
 _start:     LDR R2, =ARRAY
             
 PUSH:       LDR R1, [R2, #4]!   // load the second number from array 
-            STR R2, [SP, #-4]!  // store it at the top of the stack and move the stack pointer
-            LDR R1, [R2, #4]!   // repear above process
+            STR R1, [SP, #-4]!  // store it at the top of the stack and move the stack pointer
+            LDR R1, [R2, #4]!   // repeat above process
             STR R1, [SP, #-4]!
             LDR R1, [R2, #4]!
             STR R1, [SP, #-4]! // store the last three numbers in the array
 
-POP:        LDR R0, [SP], #4
-            LDR R0, [SP], #4
+POP:        LDR R0, [SP], #4 // stack pointer pointing to last stacked
+            LDR R0, [SP], #4 // load top of stack into R0, popping top of stack then increment pointer
             LDR R0, [SP], #4 // load the same three numbers out of the stack in order from last in first out (reverse)
 
 END:        B END
