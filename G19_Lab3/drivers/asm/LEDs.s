41960642c6af79d60ad1159f216dc44e68946386
sarahswanson <35464873+sarahswanson@users.noreply.github.com>
2018-10-31 18:31:30 -0400

    Update LEDs.s

--- G19_Lab3/drivers/asm/LEDs.s
+++ G19_Lab3/drivers/asm/LEDs.s
@@ -3,13 +3,13 @@
 			.global read_LEDs_ASM
 			.global write_LEDs_ASM
 
-read_LEDs_ASM:
-			LDR R1, =LED_BASE
+read_LEDs_ASM:					//this subroutine will load the value at the LEDs memory
+			LDR R1, =LED_BASE	//location into R0 and then branch to LR
 			LDR R0, [R1]
 			BX LR
 
-write_LEDs_ASM:
-			LDR R1, =LED_BASE
+write_LEDs_ASM:					//this subroutine will store the value in R0 at the
+			LDR R1, =LED_BASE	//LEDs memory location, then branch to LR
 			STR R0, [R1]
 			BX LR
 
