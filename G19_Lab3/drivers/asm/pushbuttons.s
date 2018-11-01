398d253626bbfb1d45a2424a58bd041f847f2d2a
sarahswanson <35464873+sarahswanson@users.noreply.github.com>
2018-11-01 15:00:53 -0400

    Update pushbuttons.s

--- G19_Lab3/drivers/asm/pushbuttons.s
+++ G19_Lab3/drivers/asm/pushbuttons.s
@@ -24,13 +24,13 @@ PB_data_is_pressed_ASM:				//this subroutine only access pushbutton data registe
 			MOVNE R0, #0		// if R2 != R0 move 0 into R0 to return false 
 			BX LR
 			
-read_PB_edgecap_ASM:				//this subroutine on access the pushbutton edgecapture register
+read_PB_edgecap_ASM:				//this subroutine only access the pushbutton edgecapture register
 			LDR R1, =PB_EDGECAP
 			LDR R0, [R1]		// load the data into R0 
 			AND R0, R0, #0xF	// return only the edge bits which is the last 4
 			BX LR 				// return data and branch back
 
-PB_edgecap_is_pressed_ASM:			//this subroutine on access the pushbutton edgecapture register
+PB_edgecap_is_pressed_ASM:			//this subroutine only access the pushbutton edgecapture register
 			LDR R1, =PB_EDGECAP	// repeat the process as above except for PB_EDGECAP
 			LDR R1, [R1]		
 			AND R2, R2, R0
@@ -39,19 +39,19 @@ PB_edgecap_is_pressed_ASM:			//this subroutine on access the pushbutton edgecapt
 			MOVNE R0, #0		
 			BX LR
 			
-PB_clear_edgecp_ASM:				//this subroutine on access the pushbutton edgecapture register
+PB_clear_edgecp_ASM:				//this subroutine only access the pushbutton edgecapture register
 			LDR R1, =PB_EDGECAP 	//load the address
 			MOV R0, #0xF		// move all 1's to address of edgecapture in order to reset it 
 			STR R0, [R1]
 			BX LR
 			
-enable_PB_INT_ASM:				//this subroutine on access the pushbutton interrupt mask register
+enable_PB_INT_ASM:				//this subroutine only access the pushbutton interrupt mask register
 			LDR R1, =PB_MASK	
 			AND R2, R0, #0xF	// we only want the last 4 numbers since there are b
 			STR R2, [R1]		// store it back into the PB_MASK to enable
 			BX LR			
 
-disable_PB_INT_ASM:				//this subroutine on access the pushbutton interrupt mask register
+disable_PB_INT_ASM:				//this subroutine only access the pushbutton interrupt mask register
 			LDR R1, =PB_MASK
 			LDR R2, [R1]		// load all the mask bits
 			BIC R1, R1, R0		// clear all the bits corresponding to R0
